d9f255d0535f8af403b552729de6cb5c7ef709af
juanchiquiza <juancas155@gmail.com>
2016-07-12 11:06:05 -0500

    porfin

--- app/src/main/java/com/healthyheart/Usuario/UsuarioConsultas.java
+++ app/src/main/java/com/healthyheart/Usuario/UsuarioConsultas.java
@@ -32,7 +32,7 @@ public class UsuarioConsultas {
 
     public String GuardarUsuario(UsuarioModel user,AdministradorBase admin) {
 
-            String Resultado="Se guardo exitosamente122";
+            String Resultado="Se guardo exitosamente12233";
             SQLiteDatabase bd = admin.getWritableDatabase();
 
             ContentValues registro = new ContentValues();
